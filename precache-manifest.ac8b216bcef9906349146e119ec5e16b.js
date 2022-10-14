13d7b649b30ec6c1247aedcfb824d7a35231672a
kenanAST <kenanjoshua3454@gmail.com>
2022-10-14 09:45:35 +0800

    initial

---
+++ precache-manifest.ac8b216bcef9906349146e119ec5e16b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ad891f8228cab5ff3c8888535e836e",
+    "url": "http://localhost:4000/index.html"
+  },
+  {
+    "revision": "a11cf7ec9c4b3c06a073",
+    "url": "http://localhost:4000/static/css/2.e4cd82dd.chunk.css"
+  },
+  {
+    "revision": "51db0714dfe305823b79",
+    "url": "http://localhost:4000/static/css/main.536df03a.chunk.css"
+  },
+  {
+    "revision": "a11cf7ec9c4b3c06a073",
+    "url": "http://localhost:4000/static/js/2.9b88fc86.chunk.js"
+  },
+  {
+    "revision": "51db0714dfe305823b79",
+    "url": "http://localhost:4000/static/js/main.081431b3.chunk.js"
+  },
+  {
+    "revision": "52d9fd6c82c3f49d9c41",
+    "url": "http://localhost:4000/static/js/runtime~main.b2d67053.js"
+  },
+  {
+    "revision": "9b5a0eca874b78bbab683a7e1999b18b",
+    "url": "http://localhost:4000/static/media/index.9b5a0eca.less"
+  }
+]);
